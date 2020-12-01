228940351a9cc4be54cba8aefa10a5c1ba10fa31
Jessica <71885306+JessiejkD@users.noreply.github.com>
2020-11-30 22:59:27 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -274,7 +274,33 @@ resetBox()
 forever(function () {
     if (box.overlapsWith(pinkButton)) {
         pinkButton.say("Scanning")
-        game.showLongText("", DialogLayout.Bottom)
+        game.setDialogFrame(img`
+            ..bbbbbbbbbbbbbbbbbbbb..
+            .bd111111111111111111db.
+            bd1dbbbbbbbbbbbbbbbbd1db
+            b1dbbbbbbbbbbbbbbbbbbd1b
+            b1bd1111111111111111db1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1b111111111111111111b1b
+            b1bd1111111111111111db1b
+            bd1bbbbbbbbbbbbbbbbbb1db
+            bbd111111111111111111dbb
+            .bbbbbbbbbbbbbbbbbbbbbb.
+            ..bbbbbbbbbbbbbbbbbbbb..
+            `)
+        game.showLongText(objectMaterial, DialogLayout.Center)
     } else if (box.overlapsWith(blueButton)) {
         blueButton.say("Scanning")
     }
